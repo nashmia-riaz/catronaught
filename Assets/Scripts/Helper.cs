f84cc0f414fef88019fc833755db2a64c3986067
Panash <67746925+p4nash@users.noreply.github.com>
2022-08-05 00:26:58 +0500

    Added base files, animated menu scene

---
+++ Assets/Scripts/Helper.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Helper
+{
+    public delegate void basicFunction();
+    public static IEnumerator waitBeforeExecution(float time, basicFunction toExecute)
+    {
+        float timer = 0;
+        while (timer < time)
+        {
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        toExecute();
+    }
+}
---
+++ Assets/Scripts/UIHandler.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIHandler : MonoBehaviour
+{
+    [SerializeField]
+    GameObject helmet, backpack;
+
+    [SerializeField]
+    SpriteRenderer text1, text2;
+
+    [SerializeField]
+    Sprite replacementText1, replacementText2;
+
+    [SerializeField]
+    Animator menuAnimator, martyAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClickPlay()
+    {
+        menuAnimator.SetTrigger("Collision");
+        StartCoroutine(Helper.waitBeforeExecution(5.5f, () => {
+            //helmet.SetActive(true);
+            //backpack.SetActive(true);
+
+            text1.sprite = replacementText1;
+            text2.sprite = replacementText2;
+
+            martyAnimator.SetTrigger("Panic");
+        }));
+    }
+}
