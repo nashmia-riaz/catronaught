42ca333f807303b581f7c45140254be62d111834
Panash <67746925+p4nash@users.noreply.github.com>
2022-08-14 05:09:42 +0500

    Added highscore system, game over case

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -11,6 +12,17 @@ public class GameManager : MonoBehaviour
 
     [SerializeField]
     StarSpawner starSpawner;
+
+    [SerializeField]
+    Vector2 initialPlayerPosition, currentPlayerPosition, distanceCovered = Vector2.zero;
+
+    [SerializeField]
+    Text scoreText;
+
+    public bool isGameOver = false;
+
+    [SerializeField]
+    float currentHighscore;
 
     private void Awake()
     {
@@ -23,13 +35,30 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        initialPlayerPosition = currentPlayerPosition = player.transform.position;
+        currentHighscore = PlayerPrefs.GetFloat("Highscore");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isGameOver) return;
+
+        if (player.transform.position.x > currentPlayerPosition.x)
+        {
+            currentPlayerPosition = player.transform.position;
+            distanceCovered = currentPlayerPosition - initialPlayerPosition;
+            scoreText.text = distanceCovered.x.ToString();
+        }
+
+    }
+
+    public void GameOver()
+    {
+        if (distanceCovered.x > currentHighscore)
+        {
+            PlayerPrefs.SetFloat("Highscore", distanceCovered.x);
+        }
     }
 
     public void RotateClockwise()
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -14,25 +14,39 @@ public class PlayerScript : MonoBehaviour
     [Range(0, 0.1f)]
     float thrusterForce;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
+    bool isPlayerDead = false;
 
     // Update is called once per frame
     void Update()
     {
+        if (isPlayerDead) return;
+
         transform.Rotate(0, 0, RPM * Time.deltaTime* (rotateAntiClockwise ? 1 : -1));
         transform.position += transform.up * thrusterForce;
     }
 
     public void RotateClockwise()
     {
+        if (isPlayerDead) return;
         rotateAntiClockwise = false;
     }
 
     public void RotateAntiClockwise()
     {
+        if (isPlayerDead) return;
         rotateAntiClockwise = true;
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Obstacle")
+        {
+            isPlayerDead = true;
+            Vector2 forceDirection = collision.transform.position - transform.position;
+            gameObject.GetComponent<Rigidbody2D>().AddForce(-forceDirection * 2, ForceMode2D.Impulse);
+            gameObject.GetComponent<Rigidbody2D>().angularVelocity = 300f;
+            GameManager.instance.GameOver();
+        }
+    }
+
 }
